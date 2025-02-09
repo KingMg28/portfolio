24d09efcc5d577efa6e6f5eda87e80b9afae94e6
KingMG <kingmg320@gmail.com>
2025-02-09 08:23:41 +0330

    update ProjectsSection styles and image dimensions

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -20,29 +20,31 @@ const ProjectsSection = () => {
           ({ id, image, description, title, gitHubLink, link, tags }) => (
             <div
               key={id}
-              className="project-item relative w-fit  bg-neutral-900  border border-neutral-700 p-4 rounded-xl shadow-lg hover:shadow-pink-800 hover:scale-105 transition-all duration-500"
+              className="project-item relative  w-fit h-fit  bg-neutral-900  border border-neutral-700 p-4 rounded-xl shadow-lg hover:shadow-pink-800 hover:scale-105 transition-all duration-500"
             >
               <Image
                 src={image}
                 alt={title}
-                width={380}
+                width={500}
                 height={500}
-                className="rounded-lg max-h-[385px] w-full object-center"
+                className="rounded-lg  max-h-[400px]"
               />
-              <h3 className="text-xl font-semibold my-2">{title}</h3>
-              <p className="text-neutral-500 text-sm">{description}</p>
-              <div className="absolute inset-0 mt-24 flex items-center justify-center gap-2 text-black text-3xl opacity-0 hover:opacity-100 transition-opacity duration-300">
-                <div className="border border-neutral-800 rounded-full p-2 bg-neutral-300 hover:bg-neutral-500">
+              <div className="absolute inset-0 flex items-center justify-center gap-2 text-black text-3xl opacity-0 hover:opacity-100 transition-opacity duration-300">
+                <div className=" border border-neutral-800 rounded-full p-2 bg-neutral-300 hover:bg-neutral-500">
                   <a href={link}>
                     <FaEye />
                   </a>
                 </div>
-                <div className="border border-neutral-800 rounded-full p-2 bg-neutral-300 hover:bg-neutral-500">
+                <div className=" border border-neutral-800 rounded-full p-2 bg-neutral-300 hover:bg-neutral-500">
                   <a href={gitHubLink}>
                     <FaCode />
                   </a>
                 </div>
               </div>
+
+              <h3 className="text-xl font-semibold my-2">{title}</h3>
+              <p className="text-neutral-500 text-sm">{description}</p>
+
               <div className="flex flex-wrap gap-1">
                 {tags?.map((tag, index) => (
                   <div
